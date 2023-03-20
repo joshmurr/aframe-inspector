55afaa25bf2a7793503b85fd4666a4a89e56b91e
Josh Murr <joshuadavidmurr@gmail.com>
2023-03-20 17:05:18 +0000

    Add checkbox for save-pos-from-vr-movements but possible not used...

--- src/components/scenegraph/BottomBar.js
+++ src/components/scenegraph/BottomBar.js
@@ -5,7 +5,8 @@ export default class BottomBar extends React.Component {
     super(props);
     const respawnLimit = localStorage.getItem("respawnLimit") || -1;
     this.state = {
-      respawnLimit
+      respawnLimit,
+      savePositionFromVR: false
     };
   }
 
@@ -19,21 +20,41 @@ export default class BottomBar extends React.Component {
     localStorage.setItem("respawnLimit", this.state.respawnLimit);
   };
 
+  updateSavePositionFromVR = e => {
+    this.setState({
+      savePositionFromVR: e.target.checked
+    });
+    console.log(this.state);
+  };
+
   render() {
     return (
       <div id="bottombar">
-        <label htmlFor="quantity">Respawn if fallen below:</label>
-        <input
-          type="number"
-          id="falling-respawn-limit"
-          name="falling-respawn-limit"
-          min="-1"
-          max="1000"
-          value={this.state.respawnLimit}
-          onChange={this.updateRespawnLimit}
-          onBlur={this.saveToLocalStorage}
-          onSubmit={this.saveToLocalStorage}
-        />
+        <div className="item">
+          <label htmlFor="quantity">Respawn if fallen below:</label>
+          <input
+            type="number"
+            id="falling-respawn-limit"
+            name="falling-respawn-limit"
+            min="-1"
+            max="1000"
+            value={this.state.respawnLimit}
+            onChange={this.updateRespawnLimit}
+            onBlur={this.saveToLocalStorage}
+            onSubmit={this.saveToLocalStorage}
+          />
+        </div>
+
+        <div className="item">
+          <label htmlFor="savePositionFromVR">
+            Save changes from VR movement?
+          </label>
+          <input
+            type="checkbox"
+            id="save-position-from-vr"
+            name="save-position-from-vr"
+          />
+        </div>
       </div>
     );
   }
